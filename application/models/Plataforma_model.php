6a05cec5a27f9e9a91d5c9d2fce71b5b651bb220
Eduardo Evaristo <91930037+edueevaristo@users.noreply.github.com>
2022-06-30 23:02:25 -0300

    updates

--- application/models/Plataforma_model.php
+++ application/models/Plataforma_model.php
@@ -4,43 +4,43 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Plataforma_model extends CI_Model {
 
-    //LISTAR
+//     //LISTAR
     public function index() 
     {
-        $this->db->order_by("plataforma");
-        return $this->db->get("plataforma")->result_array();
+        //$this->db->order_by("plataforma");
+        //return $this->db->get("plataforma")->result_array();
         
     }
 
 
-    //INSERIR
-    public function inserir($plataforma)
-    {
-        return $this->db->insert("plataforma", $plataforma);
-    }
+//     //INSERIR
+//     public function inserir($plataforma)
+//     {
+//         return $this->db->insert("plataforma", $plataforma);
+//     }
 
 
-    //ATUALIZAR
-    public function mostrar($id)
-    {
-        return $this->db->get_where('plataforma', array('id' => $id))->row_array();
-    }
-
-    
-    public function editar($id, $plataforma)
-    {
-        //$this->db->update('plataforma', $id->'id');
-        $this->db->set($plataforma);
-        $this->db->where('id', $id);
-        $this->db->update('plataforma');
-    }
+//     //ATUALIZAR
+//     public function mostrar($id)
+//     {
+//         return $this->db->get_where('plataforma', array('id' => $id))->row_array();
+//     }
 
     
-    //DELETAR
-    public function deletar($id)
-    {
-        $this->db->where("id", $id);
-        return $this->db->delete('plataforma');
-    }
+//     public function editar($id, $plataforma)
+//     {
+//         //$this->db->update('plataforma', $id->'id');
+//         $this->db->set($plataforma);
+//         $this->db->where('id', $id);
+//         $this->db->update('plataforma');
+//     }
+
+    
+//     //DELETAR
+//     public function deletar($id)
+//     {
+//         $this->db->where("id", $id);
+//         return $this->db->delete('plataforma');
+//     }
 
 }
